d78b1c626c65eb2e57c3cf1461542adb155ee943
Sebastian Sanhueza <sebastian.sanhueza.t@usach.cl>
2022-11-03 14:08:36 -0300

    se crean los archivos bases para trabajar

---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log(`API URL http://lasdfas.com/adsfad?api_key=${API_KEY}`)
+
+
+/* 
+Tenemos un archivo ignorado en el git ignore, sí desea usar este repositorio crear su APIKEY en la api que usaremos
+# Secrets.js (API KEY)
+src/secrets.js
+*/ 
