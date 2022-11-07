d16a993fdd01a389e1d2b1034f319b8280ead3c5
Sebastian Sanhueza <sebastian.sanhueza.t@usach.cl>
2022-11-07 20:25:07 -0300

    Se añade el codigo js de  las categorias

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+/*
+He aquí la documentación de la Api
+https://developers.themoviedb.org/3/getting-started
+*/
+
 
 async function getTrendingMoviesPreview() {
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=' + API_KEY);
@@ -23,9 +28,37 @@ async function getTrendingMoviesPreview() {
 
 getTrendingMoviesPreview();
 
+async function getCategoriesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
 
+    const categories = data.genres;
+    console.log({data, categories});
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container'); //classList para crear la clase del elemento html
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id); //setAttribute para agregar atributos a la etiqueta html
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    });
+}
+
+getCategoriesPreview()
 
 /* 
+Otra manera de acceder a los atributos es llamarlos como si fueran campos (visualmente se me hace mas fácil esta manera):
+movieImg.alt=“Nombre de la película”;
+movieImg.src='https://image.tmdb.org/t/p/w300/’+movie.poster_path;
+
+
 Tenemos un archivo ignorado en el git ignore, sí desea usar este repositorio crear su APIKEY en la api que usaremos
 # Secrets.js (API KEY)
 src/secrets.js
