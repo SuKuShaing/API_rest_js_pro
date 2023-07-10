750d6bff4f9aba2b88be2c78a96cd7043c6acb39
Sebastian Sanhueza01~ <sebastian.sanhueza.t@usach.cl>
2023-07-10 13:49:26 -0400

    DEPLOY proyecto a github

---
+++ src/secrets.js
@@ -0,0 +1,11 @@
+const API_KEY = '1effc82eecad3599ac339740b9aba7f2';
+
+/*
+Clave de la API (v3 auth)
+1effc82eecad3599ac339740b9aba7f2
+
+Token de acceso de lectura a la API (v4 auth)
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZWZmYzgyZWVjYWQzNTk5YWMzMzk3NDBiOWFiYTdmMiIsInN1YiI6IjYzNjg1M2IxNjY1NjVhMDA4YWMxYjcwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C_ctZCMXkQs5hQON_0e_ahJiMKnNT6pQnb8RZRyrTFw
+
+https://api.themoviedb.org/3/movie/550?api_key=1effc82eecad3599ac339740b9aba7f2
+*/
